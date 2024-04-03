433b0bd36987ca56e50bc68df20312cc0507308c
Randy Cantillo <randycantillo@Randys-MacBook-Pro.local>
2024-04-03 12:25:31 -0400

    menu testing

--- app/menu/coffee/page.tsx
+++ app/menu/coffee/page.tsx
@@ -1,3 +1,76 @@
-export default function Coffee() {
-    return 'Coffee here!';
-}
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+const products = [
+    {
+      id: 1,
+      name: 'Americano',
+      href: '#',
+      price: '$3.75',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 2,
+      name: 'Latte',
+      href: '#',
+      price: '$4.50',
+      imageSrc: 'https://images.unsplash.com/photo-1568046562322-0bbc869368ba?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      name: 'Cappuccino',
+      href: '#',
+      price: '$4.50',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      name: 'Espresso',
+      href: '#',
+      price: '$3.00',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    // More products...
+  ]
+  
+  export default function Coffee() {
+    return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-1 py-0 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
+  
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => (
+              <a key={product.id} href={product.href} className="group">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                  <img
+                    src={product.imageSrc}
+                    alt={product.imageAlt}
+                    className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
