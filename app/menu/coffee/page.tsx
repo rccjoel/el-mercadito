62c17e62907ff72a8c5884dc63bedb257607d187
Randy Cantillo <randycantillo@Randys-MacBook-Pro.local>
2024-04-04 16:01:49 -0400

    grid layout fix

--- app/menu/coffee/page.tsx
+++ app/menu/coffee/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Image from "next/image"
 
 const products = [
     {
@@ -34,7 +33,38 @@ const products = [
       imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
-    // More products...
+    {
+      id: 5,
+      name: 'Americano',
+      href: '#',
+      price: '$3.75',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+      id: 6,
+      name: 'Latte',
+      href: '#',
+      price: '$4.50',
+      imageSrc: 'https://images.unsplash.com/photo-1568046562322-0bbc869368ba?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 7,
+      name: 'Cappuccino',
+      href: '#',
+      price: '$4.50',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 8,
+      name: 'Espresso',
+      href: '#',
+      price: '$3.00',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    }
   ]
   
   export default function Coffee() {
@@ -43,7 +73,7 @@ const products = [
         <div className="mx-auto max-w-2xl lg:max-w-7xl px-3">
           <h2 className="sr-only">Products</h2>
   
-          <div className="grid grid-cols-2 gap-x-6 gap-y-10 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
             {products.map((product) => (
 
               <Link
