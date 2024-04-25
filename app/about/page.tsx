7eb97a132a009b1ea2e538f978e616b411d1fce6
Randy Cantillo <randycantillo@Randys-MacBook-Pro.local>
2024-04-25 16:26:10 -0400

    About page changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -34,40 +34,11 @@ export default function About() {
                 family and flavor. Every morning, the air crackled with the warm greetings of regulars, a mix of Leamington locals
                 and Colombian transplants drawn by the promise of a taste of home.
               </p>
-              <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                <li className="flex gap-x-3">
-                  <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">Family-owned.</strong> where quality and care are poured
-                    into every cup.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                    ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <ServerIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
-                  <span>
-                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-                  </span>
-                </li>
-              </ul>
               <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-              <p className="mt-6">
-                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                turpis ipsum eu a sed convallis diam.
+                Tatiana's journey to creating El Mercadito was one fueled by passion and perseverance. Born and raised in Colombia, 
+                she grew up surrounded by the rich flavors and vibrant colors of her homeland. However, life took her on a journey 
+                far from her roots, leading her to Leamington, where she found herself yearning for a taste of home amidst the unfamiliar 
+                surroundings.
               </p>
             </div>
           </div>
